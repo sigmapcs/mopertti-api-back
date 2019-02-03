fcd10f2a93a3ad4b8a4785ac11a307361f604d19
Florentino Navarro <tino@sigmapcs.com.mx>
2019-02-03 13:24:30 -0600

    new routes1

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,9 +14,14 @@ class ProductsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $products = Products::get();
+
+        if($request->has('category')){
+            $products = $products -> where('category',$request->get('category'));
+        }
+
         return response()->json($products,200);
     }
 
